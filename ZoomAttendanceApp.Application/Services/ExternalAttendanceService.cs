c7b11013a9d07348ca3973cb712e85391167dc70
AIZUEldor <eldor9406@icloud.com>
2025-12-29 00:00:12 +0500

    Add project files.

---
+++ ZoomAttendanceApp.Application/Services/ExternalAttendanceService.cs
@@ -0,0 +1,53 @@
+﻿using WorkingWithFileExcel.Infrastucture.Data;
+using ZoomAttendanceApp.Domain.Models;
+
+
+namespace ZoomAttendanceApp.Application.Services
+{
+    public class ExternalAttendanceService
+    {
+        private readonly ExcelContext context;
+
+        public ExternalAttendanceService()
+        {
+            context = new ExcelContext();
+        }
+
+        public List<ExternalAttendance> GetAll() => context.GetAll();
+
+        public ExternalAttendance GetByName(string name)
+        {
+            return context.GetAll()
+                .FirstOrDefault(x => x.FullNameWithCode.Contains(name, StringComparison.OrdinalIgnoreCase));
+        }
+
+        public ExternalAttendance GetByEmail(string email)
+        {
+            return context.GetAll()
+                .FirstOrDefault(x => x.Email.Contains(email, StringComparison.OrdinalIgnoreCase));
+        }
+
+        public List<ExternalAttendance> GetGuests()
+        {
+            return context.GetAll().Where(x => x.IsHost == "Да").ToList();
+        }
+
+        public List<ExternalAttendance> GetWaitingRoom()
+        {
+            return context.GetAll().Where(x => x.IsWaiting == "Да").ToList();
+        }
+
+        public int GetCount()
+        {
+            return context.GetAll().Count();
+        }
+
+        public List<ExternalAttendance> GetMostActive()
+        {
+            return context.GetAll()
+                .OrderByDescending(x => x.Duration)
+                .Take(5)
+                .ToList();
+        }
+    }
+}
